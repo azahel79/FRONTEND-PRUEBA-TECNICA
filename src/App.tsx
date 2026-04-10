e2225a3d99a2c3d7b29a34aad65c93a3d7bbf104
azahel garcia lara <azahelprogramer79@gmail.com>
2026-04-10 12:35:57 -0600

    frontend prueba tecnica

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import { useTodos } from './hooks/useTodo';
 import { TodoItem } from './components/Todoitem';
 import { Header } from './components/layout/Header';
